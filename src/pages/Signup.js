7c6a4a28287d2b0c7fac123f158abd00ff1a828d
RAJA Amara <raja.amara2019@gmail.com>
2024-10-09 23:26:18 +0200

    first

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,91 +1,69 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import SignupImage from "../asset/login-animation.gif";
 import { BiShow, BiHide } from "react-icons/bi";
-import { Link,useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ImagetoBase64 } from "../utility/ImagetoBase64";
-function Signup() {
-    const navigate = useNavigate();
+
+function Signup()
+ {
+   // const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
-    const handleShowPassword = () => {
-        setShowPassword(prev => !prev);
-    };
+    const handleShowPassword = () => setShowPassword(prev => !prev);
 
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-    const handleShowConfirmPassword = () => {
-        setShowConfirmPassword(prev => !prev);
-    };
+    const handleShowConfirmPassword = () => setShowConfirmPassword(prev => !prev);
 
-    const [data, setData] = useState({
+    const [Data, setData] = useState({
         firstname: "",
         lastname: "",
         email: "",
         password: "",
         confirmPassword: "",
-        image:"",
+        file: "",
     });
 
-    console.log(data);
-    const handleUploadProfileImage = async(e)=>{
-        const data = await ImagetoBase64(e.target.files[0])
-     console.log(data)
-  
-        setData((preve)=>{
-            return{
-              ...preve,
-              image : data
-            }
-        })
-  
-    }
+    const [img, setimg] = useState();
+
     const handleOnChange = (e) => {
         const { name, value } = e.target;
-        setData(prev => ({
-            ...prev,
-            [name]: value,
-        }));
+        setData(prev => ({ ...prev, [name]: value }));
     };
-    const handlsubmit = async (e) =>
-     {
-        e.preventDefault()
-        const { firstname, lastname, email, password, confirmPassword } = data;
-        if (firstname && lastname && email && password && confirmPassword) 
-        {
-            if(password === confirmPassword)
-            {
-            alert('succefuly');
-            navigate("/login");
-            }
+    console.log("Server URL:", process.env.REACT_APP_SERVER_DOMIN);
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        const datasignup = new FormData();
+        datasignup.append("firstname", Data.firstname);
+        datasignup.append("lastname", Data.lastname);
+        datasignup.append("email", Data.email);
+        datasignup.append("password", Data.password);
+        datasignup.append("confirmPassword", Data.confirmPassword);
+            datasignup.append("file", img);
         
-        else {
 
-            alert('bad creditel');
-        }
-    }
-    else
-    {
-        alert('Please insert correct information');
-    }
-     }
-     
+        console.log(Data)
+             const res = await  axios.post("http://localhost:5050/api/user/signup",datasignup)
+             console.log(res)
+      };
     return (
         <div className="p-3 md:p-4">
             <div className="w-full max-w-sm bg-white m-auto flex flex-col p-4">
                 <div className='w-20 h-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto relative'>
-                    <img  src={data.image ? data.image :  SignupImage} className="w-full h-full" alt='' />
+                    <img src={img ? img : SignupImage} className="w-full h-full" alt='' />
                     <label htmlFor="profileImage">
-            <div className="absolute bottom-0 h-1/3  bg-slate-500 bg-opacity-50 w-full text-center cursor-pointer">
-              <p className="text-sm p-1 text-white">Upload</p>
-            </div>
-            <input type={"file"} id="profileImage" accept="image/*" className="hidden" onChange={handleUploadProfileImage}/>
-          </label>
+                        <div className="absolute bottom-0 h-1/3 bg-slate-500 bg-opacity-50 w-full text-center cursor-pointer">
+                            <p className="text-sm p-1 text-white">Upload</p>
+                        </div>
+                        <input type="file" id="profileImage" accept="image/*" className="hidden" onChange={(e)=>setimg(e.target.files[0])} />
+                    </label>
                 </div>
-                <form onSubmit={handlsubmit} className="w-full py-3 flex flex-col">
+                <form onSubmit={handleSubmit} className="w-full py-3 flex flex-col">
                     <label htmlFor="firstname">First Name</label>
                     <input
                         type="text"
                         id="firstname"
                         name="firstname"
-                        value={data.firstname}
+                        value={Data.firstname}
                         onChange={handleOnChange}
                         className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-orange-300"
                     />
@@ -95,7 +73,7 @@ function Signup() {
                         type="text"
                         id="lastname"
                         name="lastname"
-                        value={data.lastname}
+                        value={Data.lastname}
                         onChange={handleOnChange}
                         className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-orange-300"
                     />
@@ -105,7 +83,7 @@ function Signup() {
                         type="email"
                         id="email"
                         name="email"
-                        value={data.email}
+                        value={Data.email}
                         onChange={handleOnChange}
                         className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-orange-300"
                     />
@@ -116,7 +94,7 @@ function Signup() {
                             type={showPassword ? 'text' : 'password'}
                             id="password"
                             name="password"
-                            value={data.password}
+                            value={Data.password}
                             onChange={handleOnChange}
                             className="w-full bg-slate-200 border-none outline-none"
                         />
@@ -131,7 +109,7 @@ function Signup() {
                             type={showConfirmPassword ? 'text' : 'password'}
                             id="confirmPassword"
                             name="confirmPassword"
-                            value={data.confirmPassword}
+                            value={Data.confirmPassword}
                             onChange={handleOnChange}
                             className="w-full bg-slate-200 border-none outline-none"
                         />
@@ -151,4 +129,4 @@ function Signup() {
     );
 }
 
-export default Signup
+export default Signup;
